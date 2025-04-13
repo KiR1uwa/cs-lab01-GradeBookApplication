e9f0cbb5fb80834aeb29acebc2a6ce3934be7a56
Kyryl <kyryl.pochoma@microsoft.wsei.edu.pl>
2025-04-13 21:37:00 +0200

    step 2

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,6 +14,8 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
 
+        public GradeBookType Type { get; set; } //2
+
         public BaseGradeBook(string name)
         {
             Name = name;
